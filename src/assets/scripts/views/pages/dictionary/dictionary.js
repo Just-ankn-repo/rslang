b4364912c745077348af7f44bbf48afe325e12c7
Valeria <Valeryia.Karzhaneuskaya@mail.ru>
2020-07-10 12:38:39 +0300

    fix: fix modal window

--- src/assets/scripts/views/pages/dictionary/dictionary.js
+++ src/assets/scripts/views/pages/dictionary/dictionary.js
@@ -1,4 +1,7 @@
-import generateContainer from "./dictionary-container";
+// import generateContainer from "./Container";
+// import checkbox from "./Checkbox";
+import items from "./Constants";
+import Control from "./Control";
 
 const Dictionary = {
   render : async () => {
@@ -18,21 +21,22 @@ const Dictionary = {
                         <div class="dictionary-tools__compact-btn"></div>
                         <div class="dictionary-tools__sorting">
                             <object title="Все слова" class="dictionary-tools__sorting_all"></object>
-                            <object title="Новые слова" class="dictionary-tools__sorting_new"></object>
-                            <object title="На изучении" class="dictionary-tools__sorting_studying"></object>
-                            <object title="Изучено" class="dictionary-tools__sorting_studied"></object>
+                            <object title="Легко" class="dictionary-tools__sorting_new"></object>
+                            <object title="Средне" class="dictionary-tools__sorting_studying"></object>
+                            <object title="Сложно" class="dictionary-tools__sorting_studied"></object>
                         </div>
                 </div>
 
-                <div class="dictionary-modal none">
-                    <object class="dictionary-modal__close-button"></object>
+                <div class="dictionary-modal">
+                    
+                    <div class="dictionary-modal__close-button"></div>
 
                     <div class="dictionary-card">
-                        <img src="https://raw.githubusercontent.com/irinainina/rslang-data/master/files/01_0009.jpg" class="dictionary-card__image">
-                        <div class="dictionary-card__transcription"><object class="dictionary-card__audio-icon"></object>[savanna]</div>
-                        <div class="dictionary-card__word">savanna</div>
-                        <div class="dictionary-card__translation">саванна</div>
-                        <div class="dictionary-card__example">bla bla bla</div>
+                        <img src="" class="dictionary-card__image">
+                        <div class="dictionary-card__transcription"><object class="dictionary-card__audio-icon"></object></div>
+                        <div class="dictionary-card__word"></div>
+                        <div class="dictionary-card__translation"></div>
+                        <div class="dictionary-card__example"></div>
                         <div class="dictionary-modal__arrows">
                             <object class="dictionary-modal__button_left"></object>
                             <object class="dictionary-modal__button_right"></object>
@@ -53,48 +57,8 @@ const Dictionary = {
   },    
   
   after_render: async () => {
-
-    function getWords() {
-        const url = `https://afternoon-falls-25894.herokuapp.com/words?page=0&group=0`;
-        return fetch(url)
-          .then((res) => res.json())
-          .then((data) => {
-            const div = generateContainer(data);
-            document.getElementById('content').append(div);
-          });
-      };
-
-      getWords()
-
-    document.querySelector('.dictionary-modal__close-button').addEventListener('click', () => {
-        console.log('click')
-        document.querySelector('.dictionary-modal').classList.add('none');
-    })
-
-    //! CHECKBOX
-        for(let i=0; i < document.querySelectorAll('.dictionary-element__checkbox').length; i+=1) {
-            document.querySelectorAll('.dictionary-element__checkbox')[i].onclick = () => {
-                const allChecked = document.querySelectorAll('.dictionary-element__checkbox:checked').length;
-                document.getElementById('checkAll').checked = allChecked === document.querySelectorAll('.dictionary-element__checkbox').length;
-                document.getElementById('checkAll').indeterminate = allChecked > 0 
-                && allChecked < document.querySelectorAll('.dictionary-element__checkbox').length;
-            }
-        }
-        
-        document.getElementById('checkAll').onclick = () => {
-            for(let i=0; i<document.querySelectorAll('.dictionary-element__checkbox').length; i+=1) {
-                document.querySelectorAll('.dictionary-element__checkbox')[i].checked = 
-                !document.querySelectorAll('.dictionary-element__checkbox')[i].checked;
-            }
-        }
-
-
-
-
-
-
-
-        
+      const control = new Control;
+      control.getItems(items);
   }   
 }
 
